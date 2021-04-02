610155c41271ad3a725faa3caa4aa3f2da18e450
mokumus <rocketpolka@gmail.com>
2021-04-02 16:33:50 +0300

    main.c

---
+++ main.c
@@ -0,0 +1,34 @@
+
+//
+//  main.c
+//  Signals and Process
+//
+//  Created by Muhammed Okumuş on 02.04.2021.
+//  Copyright © 2021 Muhammed Okumuş. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <ctype.h>
+#include <math.h>
+#include <errno.h>
+
+
+
+/*--------------------------GLOBALS---------------------------*/
+pid_t pid[8];
+sig_atomic_t exit_requested = 0;  
+
+/* -----------------------PROTOTYPES--------------------------*/
+
+int main(int argc, char * argv[]){
+	
+
+	return 0;
+}
