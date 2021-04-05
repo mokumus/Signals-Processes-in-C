b2440f61098f277d5c11fbb8f960ff9260e5092c
mokumus <rocketpolka@gmail.com>
2021-04-05 15:39:07 +0300

    git ignore updated

--- main.c
+++ main.c
@@ -10,14 +10,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
-#include <ctype.h>
-#include <math.h>
 #include <errno.h>
 
 /*--------------------------DEFINES---------------------------*/
